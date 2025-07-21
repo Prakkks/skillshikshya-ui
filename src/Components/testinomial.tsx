2ef88fb7d4607eef15b3970aa29003a9255a8c99
Prakkks <tprakrity@gmail.com>
2025-07-22 02:13:42 +0545

    adding tailwind base utlities again

--- src/Components/testinomial.tsx
+++ src/Components/testinomial.tsx
@@ -18,7 +18,7 @@ const Testinomial = ({ id, disablehover = false }: Props) => {
         
       <img src={task?.icons} className={` hidden z-10 md:block h-full absolute vertical-float ${task?.icon_position === 'left' ? '-left-14' : '-right-12'} top-16  `}   />
         
-       <div className={` max-w-full md:max-w-[80%]  ` } style={{float: float_value }} >
+       <div className={` max-w-full md:max-w-[80%] ` } style={{float: float_value }} >
         <h1 className=" font-bold text-xl sm:text-3xl mb-2 sm:mb-3 "> {task?.heading} </h1>
         <p className="  text-lg sm:text-xl  mb-3 sm:mb-6 "> {task?.sub_heading}</p>
         <p className=" text-md font-extralight text-gray-200 sm:text-lg md:whitespace-pre-line  " > {task?.description}</p>
